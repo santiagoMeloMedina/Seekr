9d2f6b53c787f3a7ea9c584a290bcb01e76a9e56
santiago <smelomedina05@gmail.com>
2020-05-14 23:24:02 -0500

    added requirement flask to setup

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/santiagoMeloMedina/Seekr",
     packages=["seekr"],
+    install_requires=['Flask==1.1.2'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
